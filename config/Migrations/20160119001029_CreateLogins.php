436a630b3cd2d03c0a91dbbc1f27d670a33b396b
cjquinn <christyjquinn@gmail.com>
2016-01-19 00:12:13 +0000

    Added logins migration

---
+++ config/Migrations/20160119001029_CreateLogins.php
@@ -0,0 +1,48 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateLogins extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('logins');
+        $table->addColumn('email', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addIndex(['email'], [
+            'unique' => true
+        ]);
+        $table->addColumn('password', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('token', 'string', [
+            'default' => null,
+            'limit' => 500,
+            'null' => true,
+        ]);
+        $table->addColumn('token_sent', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}

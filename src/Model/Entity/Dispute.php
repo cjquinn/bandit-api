5aa122b8d2f13533829d25ae1a871760d661c291
Christy Quinn <christy@wearelighthouse.com>
2018-06-08 17:09:41 +0100

    Updated docblocks

--- src/Model/Entity/Dispute.php
+++ src/Model/Entity/Dispute.php
@@ -8,13 +8,13 @@ use Cake\ORM\Entity;
  * @property int $match_id
  * @property int $player_a_score
  * @property int $player_b_score
+ * @property string $message
  * @property boolean $is_resolved
  * @property \Cake\I18n\Time $created
  * @property \Cake\I18n\Time $modified
  */
 class Dispute extends Entity
 {
-
     protected $_accessible = [
         '*' => false
     ];

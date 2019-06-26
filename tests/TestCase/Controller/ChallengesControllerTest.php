6dfe74defc5189713abf59900b1a980703b8a710
Christy Quinn <christyjquinn@gmail.com>
2019-06-26 17:49:06 +0100

    Added function stubs for challenges endpoints tests

---
+++ tests/TestCase/Controller/ChallengesControllerTest.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Test\TestCase\Controller;
+
+use App\Test\TestCase\Controller\ControllerTestTrait;
+
+use Cake\TestSuite\IntegrationTestCase;
+
+class ChallengesControllerTest extends IntegrationTestCase
+{
+    use ControllerTestTrait;
+
+    /**
+     * @return void
+     */
+    public function testAcceptUnauthenticated()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testAcceptInvalidClubId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testAcceptInvalidChallengeId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testAcceptExistingPlayerBId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testAcceptPatch()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testAddUnauthenticated()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testAddInvalidClubId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testAddBadData()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testAddPost()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testDeleteUnauthenticated()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testDeleteInvalidClubId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testDeleteInvalidChallengeId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testDeleteInvalidPlayer()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testDeleteMatchPlayed()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testDeleteDelete()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testIndexUnauthenticated()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testIndexInvalidClubId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testIndexBadQuery()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testIndexGet()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testViewUnauthenticated()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testViewInvalidClubId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testViewGet()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testWithdrawUnauthenticated()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testWithdrawInvalidClubId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testWithdrawInvalidChallengeId()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testWithdrawInvalidPlayer()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testWithdrawMatchPlayed()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function testWithdrawPatch()
+    {
+    }
+}

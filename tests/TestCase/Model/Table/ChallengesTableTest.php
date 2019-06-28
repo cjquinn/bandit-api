a9aa1fbecd379a3492e64f076528618440cc6624
Christy Quinn <christyjquinn@gmail.com>
2019-06-28 11:11:37 +0100

    Added stubs for challenge unittests

--- tests/TestCase/Model/Table/ChallengesTableTest.php
+++ tests/TestCase/Model/Table/ChallengesTableTest.php
@@ -19,6 +19,7 @@ class ChallengesTableTest extends TestCase
      * @var array
      */
     public $fixtures = [
+        'app.challenges'
     ];
 
     /**
@@ -39,5 +40,79 @@ class ChallengesTableTest extends TestCase
         unset($this->Challenges);
 
         parent::tearDown();
+    }
+
+    /**
+     * @return void
+     */
+    public function testAccept()
+    {
+        $challenge = $this->Challenges->get(1);
+        $playerId = 1;
+
+        $this->Challenges->accept($challenge, $playerId);
+
+        $this->assertTrue($this->Challenges->exists(['id' => $challenge->id, 'player_b_id' => $playerId]));
+
+        // Should send email to player a saying that the challenge has been accepted
+    }
+
+    /**
+     * @return void
+     */
+    public function testPatchEntityAdd()
+    {
+        // Test validation
+        // Required
+        // Empty
+        // Invalid match_datetime
+
+        // Test method
+        // club_id set
+        // player_a_id set
+    }
+
+    /**
+     * @return void
+     */
+    public function testSoftDelete()
+    {
+        // Check deleted is set
+
+        // If match was accepted send email to player b
+
+            // If time is less than 24 hours negative rep to player a
+    }
+
+    /**
+     * @return void
+     */
+    public function testFindFiltered()
+    {
+        // Shouldn't include ones where match_datetime has passed
+        // Find by open
+        // Find by accepted
+    }
+
+    /**
+     * @return void
+     */
+    public function testFindByPlayerId()
+    {
+        // should include ones where match_datetime has passed
+        // Where player_a_id OR player_b_id matches passed playerId
+        // Add all case
+    }
+
+    /**
+     * @return void
+     */
+    public function testWithdraw()
+    {
+        // Ensure player b is removed
+
+        // Should send email to player a saying player b has withdrawn
+
+        // If time is less than 24 hours negative rep to player b
     }
 }
